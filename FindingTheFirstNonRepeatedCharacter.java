06c5b476fc15b6bcb6dfdc1f1e89d6e1816daf39
samsam <100060762+Saman613@users.noreply.github.com>
2022-02-22 02:12:16 +0800

    Add files via upload

---
+++ FindingTheFirstNonRepeatedCharacter.java
@@ -0,0 +1,29 @@
+  package samFindingTheFirstNonRepeatedCharacter;
+
+import java.util.*;
+
+public class FindingTheFirstNonRepeatedCharacter{
+
+	public static void main(String[] args) {
+
+		
+		  String str1 = "SammieSamSam";
+		  System.out.println("The given string is: " + str1);
+		  for (int i = 0; i < str1.length(); i++)
+		  {
+		   boolean unique = true;
+		   for (int j = 0; j < str1.length(); j++)
+		   {
+		    if (i != j && str1.charAt(i) == str1.charAt(j)) {
+		     unique = false;
+		     break;
+		    }
+		   }
+		   if (unique) {
+		    System.out.println("The first non repeated character in String is: " + str1.charAt(i));
+		    break;
+		    
+		   }
+		 }
+	}
+}
